c2e7f4e739ce424489a90adaa08f8b94daca706e
Jerome Rigault <jerome.rigault@expleogroup.com>
2021-08-24 17:33:33 +0200

    Continue Authentication activity
    style:
    * implement authentication layout
    * create themes and colors
    
    feat:
    * add Facebook specific strings with app id

--- app/src/main/java/com/jr_eagle_ocr/go4lunch/AuthenticationActivity.java
+++ app/src/main/java/com/jr_eagle_ocr/go4lunch/AuthenticationActivity.java
@@ -3,12 +3,19 @@ package com.jr_eagle_ocr.go4lunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.jr_eagle_ocr.go4lunch.databinding.ActivityAuthenticationBinding;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    private ActivityAuthenticationBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authentication);
+
+        mBinding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 }
