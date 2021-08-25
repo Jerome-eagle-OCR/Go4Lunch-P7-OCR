8858675c50f22ba1903652bb9c16b3318d2bf078
Jerome Rigault <jerome.rigault@expleogroup.com>
2021-08-25 09:59:42 +0200

    Continue Authentication activity
    feat:
    * partially implement AuthenticationActivity -> sign in page works, intent result has to be managed

--- app/src/main/java/com/jr_eagle_ocr/go4lunch/AuthenticationActivity.java
+++ app/src/main/java/com/jr_eagle_ocr/go4lunch/AuthenticationActivity.java
@@ -1,21 +1,80 @@
 package com.jr_eagle_ocr.go4lunch;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jr_eagle_ocr.go4lunch.databinding.ActivityAuthenticationBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
-    private ActivityAuthenticationBinding mBinding;
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mBinding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
+        ActivityAuthenticationBinding binding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initAuthentication();
+    }
+
+    private void initAuthentication() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Create custom layout
+        AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.activity_authentication)
+                .setGoogleButtonId(R.id.fui_google)
+                .setFacebookButtonId(R.id.fui_facebook)
+                .setTwitterButtonId(R.id.fui_twitter)
+                .setEmailButtonId(R.id.fui_email)
+                .build();
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.Theme_Go4Lunch_FirebaseUI)
+                .setLockOrientation(true)
+                .setAuthMethodPickerLayout(customLayout)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 }
